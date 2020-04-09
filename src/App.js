6cdc8777aabb504301fddbf1012af7df0a7a6328
Enderson Rufino Amorim <endersonrufinoamorim@outlook.com>
2020-04-09 19:08:20 -0300

    finalizado metodos de listar, adiconar e remover

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository(event) {
+
+    event.preventDefault();
+
+    const data = {
+      title: `Novo repositorio ${Date.now()}`,
+      url: "https://github.com/endersonra/go-stack-challenge-02-node-js-concepts",
+      techs: ["Node.js",
+        "React.js",
+        "React Native"
+      ]
+    };
+
+    const response = await api.post('repositories', data);
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repositorie => repositorie.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repositorie => (
+          <li key={repositorie.id}>
+            <p>{repositorie.title}</p>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repositorie.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
